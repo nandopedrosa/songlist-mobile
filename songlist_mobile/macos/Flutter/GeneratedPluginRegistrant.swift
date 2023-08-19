21fcb4cc76b05dad1ac0befdd8ddfca33862b6cb
Fernando Pedrosa <fpedrosa@gmail.com>
2023-08-19 13:29:04 -0300

    android build 1.0.5+13

--- songlist_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ songlist_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import in_app_purchase_storekit
 import path_provider_foundation
 import share_plus
-import shared_preferences_macos
+import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
